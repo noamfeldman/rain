9f838f0395e70567271e3f0e16d6c6efbe4722e5
noam.feldman <noam.feldman@global-e.com>
2023-01-09 19:00:04 +0200

    Add rotate animation

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,13 +2,25 @@ import React from "react";
 import { Fade } from "@mui/material";
 import logo from "../assets/logo.png";
 import { IDropsProps } from "../model/interfaces";
+import styled from "@emotion/styled";
+
+const SpinImage = styled.img`
+    width: 256px;
+    height: 256px;
+    animation:spin 1.5s linear infinite;
+
+@keyframes spin { 
+    100% { 
+        -webkit-transform: rotate(360deg); 
+        transform:rotate(360deg); 
+}}`
 
 export default function Logo(props: IDropsProps) {
     const { numOfDrops } = props;
     const display = numOfDrops === 100;
     return (
         <Fade in={display} timeout={2000}>
-            <img src={logo} alt="יצאת צדיק!" height="256" width="256"/>
+            <SpinImage src={logo} alt="יצאת צדיק!" />
         </Fade>
     )
 }
